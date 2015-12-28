970bf24fac92d6c50626b4569604a83f388bf1b3
Mohamed Dardouri <med@mahd.company>
2015-12-28 09:25:12 +0100

    fix error

--- api/submit_top.php
+++ api/submit_top.php
@@ -1,5 +1,5 @@
 <?php
-  if(!$user) die("status"=>"login_required");
+  if(!$user || $user->type!="citizen") die("status"=>"login_required");
   if(isset($_POST["elected"])){
     $elected=new elected($_POST["elected"]);
     if(!$elected->isvalid) die(json_encode(array("status"=>"invalid_elected")));
